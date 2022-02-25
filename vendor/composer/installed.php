787f18f19880a9312c8429c052aca6026becff37
GitHub Action <action@github.com>
2022-02-25 21:08:30 +0000

    Updates plugin including built assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-1a2aaec725aafa6d67899a5deda5398c098a1e81',
+        'version' => 'dev-1a2aaec725aafa6d67899a5deda5398c098a1e81',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1a2aaec725aafa6d67899a5deda5398c098a1e81',
+        'name' => 'mjbernha/vettel',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mjbernha/vettel' => array(
+            'pretty_version' => 'dev-1a2aaec725aafa6d67899a5deda5398c098a1e81',
+            'version' => 'dev-1a2aaec725aafa6d67899a5deda5398c098a1e81',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1a2aaec725aafa6d67899a5deda5398c098a1e81',
+            'dev_requirement' => false,
+        ),
+    ),
+);
